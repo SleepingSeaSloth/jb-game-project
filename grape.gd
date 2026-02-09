69563665637f4af68d8d7644a4f976f64f9a66b2
SleepingSeaSloth <seaslothstudios@gmail.com>
2026-02-08 17:31:03 -0800

    BendanaJohnBryan_Mechanics_01

--- grape.gd
+++ grape.gd
@@ -3,7 +3,7 @@ extends Area2D
 @onready var grape_power: AnimationPlayer = $GrapePower
 
 
-func _on_body_entered(_body: Node2D) -> void:
-	if "sir_knight" in _body.name:
-		_body.grape_power()
-		grape_power.play("PowerAnimation")
+#func _on_body_entered(_body: Node2D) -> void:
+	#if "sir_knight" in _body.name:
+		#_body.grape_power()
+		#grape_power.play("PowerAnimation")
