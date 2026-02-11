70a8fa25bd3bcad9dcfd70affdc6800f9cef16cf
SleepingSeaSloth <seaslothstudios@gmail.com>
2026-02-11 00:01:58 -0800

    Bendana_John_Bryan_Mechanics_01

--- grape.gd
+++ grape.gd
@@ -3,7 +3,8 @@ extends Area2D
 @onready var grape_power: AnimationPlayer = $GrapePower
 
 
-#func _on_body_entered(_body: Node2D) -> void:
-	#if "sir_knight" in _body.name:
-		#_body.grape_power()
-		#grape_power.play("PowerAnimation")
+func _on_body_entered(_body: Node2D) -> void:
+	if "Sir_Knight" in _body.name:
+		var player : Knight = _body as Knight
+		player._grape_power()
+		grape_power.play("PowerAnimation")
--- sir_knight.gd
+++ sir_knight.gd
@@ -1,11 +1,10 @@
-class_name knight
-
 extends CharacterBody2D
 
+class_name Knight
 
 const SPEED : = 125.0
 const JUMP_VELOCITY : = -280.0
-const JUMP_BOOST : = -280.0
+const JUMP_BOOST : = -500.0
 
 @onready var stomping_feet: Area2D = $StompingFeet
 @onready var knight_sprite: AnimatedSprite2D = $AnimatedSprite2D
@@ -13,19 +12,21 @@ const JUMP_BOOST : = -280.0
 
 
 func _physics_process(delta: float) -> void:
-	set_collision_layer_value(7, true)
+	set_collision_mask_value(7, true)
 	
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-		if Input.is_action_just_pressed("Move Down"):
-			set_collision_layer_value(7, false)
-
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		if Input.is_action_pressed("Move Down"):
-			set_collision_layer_value(7, false)
+			set_collision_mask_value(7, false)
 		else:
 			velocity.y = JUMP_VELOCITY
+
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		if Input.is_action_pressed("Move Down"):
+			set_collision_mask_value(7, false)
+
+
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
